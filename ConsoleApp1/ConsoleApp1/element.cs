847cbc30a1e01ba65118368099c8b9a2104f410d
truongduy5998 <phamtruongduy5998@gmail.com>
2018-10-07 19:26:17 +0700

    Tai

---
+++ ConsoleApp1/ConsoleApp1/element.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class element
+    {
+        int data;
+
+        public int Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        element next;
+
+        internal element Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+        public element (int d)
+        {
+            this.data = d;
+            this.Next = null;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class stack
+    {
+        element head;
+        element tail;
+        public stack()
+        {
+            head = null;
+            tail = null;
+
+        }
+
+        public void push(element e)
+        {
+            if (head == null)
+                tail = e;
+            e.Next = head;
+            head = e;
+        }
+        public element pop()
+        {
+            element a;
+            a = head;
+            head = head.Next;
+            return a;
+
+        }
+        public element printall()
+        {
+            element travel = head;
+            while (travel != null)
+            {
+
+                Console.WriteLine(travel.Data);
+                travel = travel.Next;
+            }
+            return null;
+        }
+    }
+}
