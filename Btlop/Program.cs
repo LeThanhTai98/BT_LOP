605bab177a606c6a9295af190a281a0c5fc2b756
Thanh Tai <Thanh Tai@DESKTOP-6H9QI3M>
2018-10-22 12:41:54 +0700

    update again

--- Btlop/Program.cs
+++ Btlop/Program.cs
@@ -14,16 +14,19 @@ namespace Btlop
             vessel a = new vessel();
             int n = 3,dem=0;
             a.create_vessel(n);
+            storage b = new storage();
             for (int i=0; i < n; i++)
                 for(int j=0;j<n;j++)
             {
                     a.in_vessel(i,dem++ );
             }
-            List<int> tam = a.grab();
-            foreach ( int i in tam)
+            for (int i = 0; i < n; i++)
             {
-                Console.Write(i);
+                List<int> tam = a.grab();
+                b.catching(tam);
+                b.sapxep();
             }
+            b.showstorage();
             Console.ReadKey();
         }
     }
--- Btlop/storage.cs
+++ Btlop/storage.cs
@@ -9,7 +9,8 @@ namespace Btlop
     class storage
 
     {
-        Stack<int> A = new Stack<int>();
+        //Stack<int> [] A= new Stack<int>[99];
+        List<Stack<int>> A = new List<Stack<int>>();
         int[] mangtam = new int [99];
          List<int> tam = new List<int>();
         public void catching (List<int> tam)
@@ -19,10 +20,7 @@ namespace Btlop
         public void sapxep()
         {
             int dem = 0;
-            while (mangtam[dem] != 0)
-            {
-                dem++;
-            }
+            dem = mangtam.Count<int>();
             for(int i=0;i<dem;i++)
                 for (int j = 0; j < dem; j++)
                     if (mangtam[i] > mangtam[j])
@@ -31,9 +29,20 @@ namespace Btlop
                         mangtam[i] = mangtam[j];
                         mangtam[j] = tam2;
                     }
-
+            Stack<int> temp = new Stack<int>();
             for (int i = 0; i < dem; i++)
-                A.Push(mangtam[i]);
+                temp.Push(mangtam[i]);
+            A.Add(temp);
+        }
+        public void showstorage()
+        {
+            foreach (Stack <int> a in A)
+            {
+                while ( a.Count != 0)
+                {
+                    Console.WriteLine(a.Pop());
+                }
+            }
         }
     }
 }
