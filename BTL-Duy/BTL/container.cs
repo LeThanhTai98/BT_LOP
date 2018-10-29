8910983a6a8d383248fc74e644bf9ec1a73ca014
truongduy5998 <phamtruongduy5998@gmail.com>
2018-10-29 22:57:36 +0700

    TH3 - den bai tam

---
+++ BTL-Duy/BTL/container.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL
+{
+	class container
+	{
+		int soHieu;
+		//public container()
+		//{
+		//	SoHieu = 3;
+		//}
+		//public container(int x)
+		//{
+		//	SoHieu = x;
+		//}
+
+		public int SoHieu { get => soHieu; set => soHieu = value; }
+
+		//public void show()
+		//{
+		//	Console.WriteLine(SoHieu);
+		//}
+		
+	}
+}
---
+++ BTL-Duy/BTL/storage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTL
+{
+	class storage
+	{
+		List<Stack<int>> A = new List<Stack<int>>(); // List các Stack
+		int soLuongCot; // số cột
+		int chieuCaoStack; // chiều cao = số hàng
+
+		public int SoLuongCot { get => soLuongCot; set => soLuongCot = value; }
+		public int ChieuCaoStack { get => chieuCaoStack; set => chieuCaoStack = value; }
+		public int totalContainerInStorage() { return soLuongCot * chieuCaoStack; }
+		public bool conditional_storage(vessel myVessel)
+		{
+			if (myVessel.totalContainerInVessel() <= totalContainerInStorage() && chieuCaoStack > myVessel.ChieuCaoStack)
+			{
+				Console.WriteLine("OK");
+				return true;
+			}
+			else
+			{
+				Console.WriteLine("NOT OK");
+				return false;
+			}
+		}
+		public void create_storage()
+		{
+			for (int i = 0; i < soLuongCot; i++)
+			{
+				A.Add(new Stack<int>());
+			}
+		}
+		public void show_storage()
+		{
+			int[,] bienTam = new int[soLuongCot, chieuCaoStack];
+			for (int i = 0; i < soLuongCot; i++)
+			{
+				int[] tempArr = new int[chieuCaoStack];
+				tempArr = A[i].ToArray(); // Chuyển sang Mảng
+				for (int j = 0; j < tempArr.Length; j++)
+				{
+					bienTam[i, j] = tempArr[j];
+				}
+			}
+			for (int i = 0; i < chieuCaoStack; i++)
+			{
+				for (int j = 0; j < soLuongCot; j++)
+				{
+					Console.Write("\t" + bienTam[j, i]);
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
